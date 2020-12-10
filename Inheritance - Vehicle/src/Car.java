14727033a1d7b02ed4d47a25fa043f0e4a72d594
Mike McGuire <MoonshotMike@gmail.com>
2020-12-10 08:49:58 -0700

    Initial commit

---
+++ Inheritance - Vehicle/src/Car.java
@@ -0,0 +1,13 @@
+
+public class Car extends Vehicle
+	{
+	private String type = "car";
+	public void displaySpeed()
+		{
+		System.out.println("as fast as the car in front of me");
+		}
+	public void honkHorn()
+		{
+		System.out.println("honk honk");
+		}
+	}
---
+++ Inheritance - Vehicle/src/Vehicle.java
@@ -0,0 +1,9 @@
+
+public class Vehicle
+	{
+	private String type = "vehicle";
+	public void displaySpeed()
+		{
+		System.out.println("not fast enough");
+		}
+	}
